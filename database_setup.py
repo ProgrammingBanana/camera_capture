85c2f49b3cbc46951a1ae95d95740f93ce8ce6f7
Daniel De Jesus <daniel.jesus@upr.edu>
2022-03-22 16:24:14 -0400

    Create database_setup.py to set up initial entries

---
+++ database_setup.py
@@ -0,0 +1,21 @@
+from database import Database as DB
+
+db = DB()
+
+db.insert_sign("ayuda", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/ayuda")
+db.insert_sign("clase", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/clase")
+db.insert_sign("donde", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/donde")
+db.insert_sign("gracias", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/gracias")
+db.insert_sign("hola", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/hola")
+db.insert_sign("necesitar", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/necesitar")
+db.insert_sign("no_entender", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/no_entender")
+db.insert_sign("repetir", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/repetir")
+db.insert_sign("n-a", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/n-a")
+db.insert_sign("empty", "/Users/codingdan/Documents/University/Semestre 2 2021-2022/Tesina/codigo/ML_Camera_Capture/MP_Data/empty")
+
+
+sign_data = db.get_signs()
+print(len(sign_data))
+
+for sign in sign_data:
+    print(sign)
